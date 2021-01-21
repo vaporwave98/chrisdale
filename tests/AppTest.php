9adcf1c3780f27cbd42d25444d100e8d9e67ecfc
vaporwave98 <jimchristian@hotmail.no>
2021-01-21 16:24:19 +0100

    Added unit tests for App and Blog classes

---
+++ tests/AppTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once("class/App.php");
+
+use \PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    public $app = null;
+
+    public function setUp(): void {
+        $this->app = new App(new Smarty(), null);
+    }
+
+    public function testAppRender() {
+        $this->app->content = "phpunit";
+        $this->assertStringContainsString($this->app->content, $this->app->render());
+    }
+
+    public function testAppPrettyPrint() {
+        $this->assertMatchesRegularExpression("/<pre>example<\/pre>/", $this->app->prettyPrint("example", true));
+    }
+}
