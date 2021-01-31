226db1a11b6d2e3ad31b7cd85ef82e3e411378cf
vaporwave98 <jimchristian@hotmail.no>
2021-01-31 01:37:18 +0100

    Added support for more request methods

--- class/Router.php
+++ class/Router.php
@@ -9,27 +9,33 @@ class Router {
     }
 
     function add($path, $method, $fn) {
-        self::$routes[] = ["path" => $path, "method" => $method, "fn" => $fn];
+        self::$routes[] = ["path" => $path, "method" => strtoupper($method), "fn" => $fn];
     }
 
-    function begin() {
-        // @/+[a-z-]+/+[a-z-]+@i
-        // @{(.*?)}@
-        // @{[a-z]+}@i
+    function begin(): bool {
         $parsed = parse_url($_SERVER["REQUEST_URI"]);
 
         foreach (self::$routes as $route) {
             if (preg_match("@/+[a-z0-9]+/+([a-z0-9]+)@", $parsed["path"], $val) && preg_match("@{(.*?)}@", $route["path"], $selector)) {
                 preg_match("@/+[a-z0-9]+@", $parsed["path"], $match);
+
                 if (strpos($match[0], $route["path"]) != -1) {
-                    $this->res->attr = [$selector[1] => $val[1]];
-                    call_user_func_array($route["fn"], [$this->res]);
+                    if ($route["method"] == $_SERVER["REQUEST_METHOD"]) {
+                        $this->res->attr = [$selector[1] => $val[1]];
+                        call_user_func_array($route["fn"], [$this->res]);  
+                        return true;                      
+                    }
                 }
             } else {
                 if ($route["path"] == $parsed["path"]) {
-                    call_user_func($route["fn"]);
+                    if ($route["method"] == $_SERVER["REQUEST_METHOD"]) {
+                        call_user_func($route["fn"]);
+                        return true;
+                    }
                 }
             }
         }
+
+        return false;
     }
 }
